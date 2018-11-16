147be1fc9b0575c8ff32c8b8619b275a43956371
Javier de J. Flores Mondragón <jjflores@innsytech.com>
2018-11-15 23:50:29 -0600

    Inicialización del proyecto
    * Se agregan diccionarios
    * Se agrega *activity* para vista principal

---
+++ app/src/main/java/com/cryptowallet/app/WalletAppActivity.java
@@ -0,0 +1,15 @@
+package com.cryptowallet.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cryptowallet.R;
+
+public class WalletAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet_app);
+    }
+}
