2397d9222e0a354d17c5b7719b6b5645d27b1178
Ing. Javier Flores <jjflores@innsytech.com>
2019-04-27 00:40:25 -0500

    feat: Implementación de conexión SSL a MongoDb

--- CryptoWalletBackend/src/data-access/storage.ts
+++ CryptoWalletBackend/src/data-access/storage.ts
@@ -1,11 +1,18 @@
 import Utils from "../utils/utils";
 import { MongoClient, Collection } from "mongodb";
+import fs from "fs";
 
 export interface IConnectionOptions {
     host?: string,
     port?: number,
     dbName: string,
-    poolSize?: number
+    poolSize?: number,
+    user?: string,
+    pwd?: string,
+    ssl?: {
+        sslCA?: string,
+        sslPEM?: string
+    }
 };
 
 export type BulkUpdate<T> = {
@@ -50,9 +57,20 @@ export abstract class Storage<TModel> {
 
     public async connect() {
         try {
-            let uri = `mongodb://${this.connectionProperties.host}:${this.connectionProperties.port}/${this.connectionProperties.dbName}?socketTimeoutMS=3600000&noDelay=true`;
+            let uri = `mongodb://${this.connectionProperties.host}:${this.connectionProperties.port}/${this.connectionProperties.dbName}`;
+            let sslAuth = Utils.isNotNull(this.connectionProperties.ssl);
+            let ca = sslAuth ? fs.readFileSync(this.connectionProperties.ssl.sslCA) : null;
+            let cert = sslAuth ? fs.readFileSync(this.connectionProperties.ssl.sslPEM) : null;
+            let key = cert;
 
-            this.client = await MongoClient.connect(uri, { useNewUrlParser: true, poolSize: this.connectionProperties.poolSize });
+            this.client = await MongoClient.connect(uri, {
+                noDelay: true, socketTimeoutMS: 3600000,
+                useNewUrlParser: true, poolSize: this.connectionProperties.poolSize, ssl: sslAuth, sslCA: [ca], sslCert: cert, sslKey: key, auth: {
+                    user: this.connectionProperties.user,
+                    password: this.connectionProperties.pwd
+                },
+                authSource: this.connectionProperties.dbName
+            });
             let db = await this.client.db(this.connectionProperties.dbName);
 
             this.collectionInternal = await db.collection(this.collectionName);
