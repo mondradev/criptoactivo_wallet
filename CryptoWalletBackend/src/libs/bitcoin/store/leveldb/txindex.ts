e1d316a4e41d3ac8fba7e93cec12bd51f18bb169
Ing. Javier Flores <jjflores@innsytech.com>
2020-06-02 08:58:07 -0500

    chore(backend): Se documenta TxIndex

--- CryptoWalletBackend/src/libs/bitcoin/store/leveldb/txindex.ts
+++ CryptoWalletBackend/src/libs/bitcoin/store/leveldb/txindex.ts
@@ -1,46 +1,67 @@
 import { Transaction } from "bitcore-lib";
-import level, { LevelUp, AbstractLevelDOWN } from "level";
+import level, { LevelUp, AbstractLevelDOWN, CodecOptions } from "level";
 import { TxindexEntry } from "./txindexentry";
 import { Enconding } from "./enconding";
 import { getDirectory } from "../../../../utils";
+import { getKey } from './dbutils'
 
-const TXINDEX_PATH = "db/bitcoin/txindex"
+/**
+ * Ruta del indice de transacciones
+ */
+const TXINDEX_PATH: string = "db/bitcoin/txindex"
 
+/**
+ * Tipo de codificado para los campos del indice.
+ */
+const TXINDEX_DB_TYPE: CodecOptions = { keyEncoding: 'binary', valueEncoding: 'binary' }
+
+/**
+ * Esta clase administra el indice de Transacciones de Bitcoin. Ofrece funciones para almanacenar 
+ * y extraer el indice deseado.
+ * 
+ * @author Ing. Javier Flores <jjflores@innsytech.com>
+ * @version 2.0
+ * @see TxindexEntry
+ */
 export class TxIndex {
 
+    /**
+     * Instancia de la base de datos.
+     */
     private _db: LevelUp<AbstractLevelDOWN<Buffer, Buffer>>
 
+    /**
+     * Crea una nueva instancia del indice.
+     */
     public constructor() {
-        this._db = level(getDirectory(TXINDEX_PATH), { keyEncoding: 'binary', valueEncoding: 'binary' })
+        this._db = level(getDirectory(TXINDEX_PATH), TXINDEX_DB_TYPE)
     }
 
+    /**
+     * Obtiene el indice de la transacción especificada.
+     * @param txid Hash de la transacciones.
+     * @returns Una promesa de un indice de transacción.
+     */
     public async getIndexByHash(txid: Buffer): Promise<TxindexEntry> {
-        const raw = await this.get(Enconding.Txindex.key(txid))
-
-        if (!raw)
-            return null
-
-        return Enconding.Txindex.decode(raw)
+        return await getKey(this._db, txid, Enconding.Txindex)
     }
 
-    private async get(key: Buffer): Promise<Buffer> {
-        try {
-            return await this._db.get(key)
-        } catch (ignored) {
-            return null
-        }
-    }
-
+    /**
+     * Indexa todas las transacciones.
+     * @param transactions Conjunto de transacciones a indexar.
+     * @param blockHash Hash del bloque al que pertenecen.
+     * @returns Una promesa vacía.
+     */
     public async indexing(transactions: Transaction[], blockHash: Buffer) {
-        const batchDb = this._db.batch()
+        const dbTransaction = this._db.batch()
 
         for (const [index, transaction] of transactions.entries()) {
             const hash = transaction._getHash()
 
-            batchDb.put(Enconding.Txindex.key(hash),
+            dbTransaction.put(Enconding.Txindex.key(hash),
                 Enconding.Txindex.encode(new TxindexEntry(blockHash, index)))
         }
 
-        await batchDb.write()
+        await dbTransaction.write()
     }
 }
--- CryptoWalletBackend/src/libs/bitcoin/store/leveldb/txindexentry.ts
+++ CryptoWalletBackend/src/libs/bitcoin/store/leveldb/txindexentry.ts
@@ -1,19 +1,67 @@
 import BufferHelper from "../../../../utils/bufferhelper";
 
+/**
+ * Provee de un estructura para un indice de Transacción de Bitcoin, permitiendo
+ * que este pueda ser serializado y viceversa.
+ * 
+ * @author Ing. Javier Flores <jjflores@innsytech.com>
+ * @version 2.0
+ * @see TxIndex
+ */
 export class TxindexEntry {
 
-    public constructor(private _blockHash: Buffer, private _index: number) { }
+    /**
+     * Hash del bloque contiene la transacción.
+     */
+    private _blockHash: Buffer
 
+    /**
+     * Posición de la transacción en el bloque.
+     */
+    private _index: number
+
+    /**
+     * Crea una instancia nueva del indice.
+     * 
+     * @param blockHash Hash del bloque que contiene la transacción.
+     * @param index Posición de la transacción en el bloque.
+     */
+    public constructor(blockHash: Buffer, index: number) {
+        this._blockHash = blockHash
+        this._index = index
+    }
+
+    /**
+     * Obtiene el hash del bloque que la contiene.
+     * @returns Hash del bloque.
+     */
     public get blockHash() { return this._blockHash; }
+
+    /**
+     * Obtiene la posición de la transacción en el bloque.
+     * @returns Posición de la transacción.
+     */
     public get index() { return this._index; }
 
+    /**
+     * Serializa el indice.
+     * @returns Los bytes que representan el indice.
+     */
     public toBuffer(): Buffer {
         return BufferHelper.zero()
             .appendUInt32LE(this._index)
-            .append(this._blockHash);
+            .append(this._blockHash)
     }
-    
+
+    /**
+     * Deserializa el indice a partir de un conjunto de bytes.
+     * @param data Bytes que representan un indice de transacción.
+     * @returns Un indice de transacción.
+     */
     public static fromBuffer(data: Buffer): TxindexEntry {
-        return new TxindexEntry(data.read(32, 4), data.readUInt32LE(0));
+        if (data.length < 36)
+            throw new Error("The data must be 36 bytes")
+
+        return new TxindexEntry(data.read(32, 4), data.readUInt32LE(0))
     }
 }
