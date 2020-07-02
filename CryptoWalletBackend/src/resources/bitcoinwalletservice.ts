c6e088554893a4f864b9cbfb130a0a1c5e1ba550
Ing. Javier Flores <jjflores@innsytech.com>
2020-07-02 15:48:32 -0500

    feat(backend): Ruta block, soporte mempool

--- CryptoWalletBackend/src/resources/bitcoinwalletservice.ts
+++ CryptoWalletBackend/src/resources/bitcoinwalletservice.ts
@@ -7,10 +7,11 @@ import Config from "../../config/index"
 import NetworkStatus from "../libs/bitcoin/network/networkstatus"
 import WalletProvider from "../libs/bitcoin/wallet"
 import LoggerFactory from 'log4js'
+import { Mempool } from "../libs/bitcoin/store/leveldb/mempool"
 
 type ParamValid = { message?: string, error: boolean, code?: number }
 
-const VERSION = "v0.5-beta" // 2020-02-29
+const VERSION = "v0.6-beta" // 2020-02-29
 const VERSION_API = "v1" // 2020-04-01
 const URL_BASE = `/api/${VERSION_API}/btc/`
 
@@ -20,13 +21,24 @@ Logger.level = Config.logLevel
 Logger.info("Bitcoin node %s", VERSION)
 
 const chain = new Blockchain(new LevelStore())
-const net = new Network(chain)
-const wallet = new WalletProvider(chain, net)
+const mempool = new Mempool(chain)
+const net = new Network(chain, mempool)
+const wallet = new WalletProvider(chain, mempool, net)
 
 async function start() {
     await chain.connect()
+    await mempool.load()
     await net.connect()
     net.start()
+}
+
+function errorHandler(error: Error) {
+    Logger.error(JSON.stringify(error.message))
+
+    if (error.stack)
+        Logger.debug(error.stack)
+
+    exitHandler()
 }
 
 async function exitHandler() {
@@ -85,29 +97,38 @@ function validateHash(hash: string): ParamValid {
 
 process.on('SIGINT', exitHandler)
 process.on('SIGTERM', exitHandler)
+process.on('uncaughtException', errorHandler)
 
 const router = Router()
     .get(URL_BASE + ":network/*", async (req: Request, res: Response, next: NextFunction) => {
         const network = req.params.network;
         const networkValid = validateNetwork(network);
 
+        Logger.trace("Received request: %s", req.path.substring(0, Math.min(100, req.path.length)))
+
         if (networkValid.error)
             res.status(networkValid.code).json({ message: networkValid.message });
-        else
+        else if (req.path === URL_BASE + network + "/chaininfo")
             next()
-    })
-    .get(URL_BASE + ":network/((?!chaininfo).)*", async (req: Request, res: Response, next: NextFunction) => {
-        if ((await net.getStatus()) < NetworkStatus.SYNCHRONIZED)
+        else if ((await net.getStatus()) < NetworkStatus.SYNCHRONIZED)
             res.status(423).json({ message: 'Bitcoin Synchronizing' });
         else
             next();
+    })
+    .get(URL_BASE + ':network/chaininfo', async (req: Request, res: Response, next: NextFunction) => {
+        const network: string = req.params.network
+
+        Logger.debug("Request received [Op=chaininfo, Param={ network: %s }]", network)
+
+        res.status(200).json(await wallet.getChainInfo(network))
     })
     .get(URL_BASE + ":network/history/:addresses", async (req: Request, res: Response, next: NextFunction) => {
         const height = parseInt(req.query.height as string) >> 0
         const addresses: string = req.params.addresses
         const network = req.params.network;
 
-        Logger.debug("Request received [Op=history, Param={ network: %s }]", network)
+        Logger.debug("Request received [Op=history, Param={ addresses: %s, network: %s }]",
+            "byte[" + addresses.length / 2 + "]", network)
 
         if (addresses.length < 42)
             res.status(400).json({ message: "Any address wasn't specified" })
@@ -128,13 +149,6 @@ const router = Router()
         else
             res.status(200).json(await wallet.getHistoryByAddress(address, network, height))
     })
-    .get(URL_BASE + ':network/chaininfo', async (req: Request, res: Response, next: NextFunction) => {
-        const network: string = req.params.network
-
-        Logger.debug("Request received [Op=chaininfo, Param={ network: %s }]", network)
-
-        res.status(200).json(await wallet.getChainInfo(network))
-    })
     .get(URL_BASE + ":network/tx/:txid", async (req: Request, res: Response, next: NextFunction) => {
         const txid: string = req.params.txid
         const network: string = req.params.network
@@ -144,8 +158,13 @@ const router = Router()
 
         if (hashValid.error)
             res.status(hashValid.code).json({ message: hashValid.message })
-        else
-            res.status(200).json(await wallet.getTransaction(txid, network))
+        else {
+            let tx = await wallet.getTransaction(txid, network)
+
+            tx = tx || await wallet.getTransactionFromMempool(txid, network)
+
+            res.status(200).json(tx)
+        }
     })
     .get(URL_BASE + ":network/txdeps/:txid", async (req: Request, res: Response, next: NextFunction) => {
         const txid: string = req.params.txid
@@ -159,11 +178,26 @@ const router = Router()
         else
             res.status(200).json(await wallet.getTxDependencies(txid, network))
     })
-    .get(URL_BASE + ":network/broadcast/:hex", async (req: Request, res: Response, next: NextFunction) => {
-        const raw = req.params.hex
+    .put(URL_BASE + ":network/broadcast", async (req: Request, res: Response, next: NextFunction) => {
+        const raw = req.body.hex || ""
         const network = req.params.network
 
-        res.status(200).json({ sent: await wallet.broadcastTx(raw, network) })
+        Logger.debug("Request received [Op=broadcastTx, Param={ raw: %s, network: %s }]", "byte[" + raw.length / 2 + "]", network)
+
+        if (raw.length < 24)
+            res.status(400).json({ message: "Any transaction wasn't specified" })
+        else
+            res.status(200).json({ sent: await wallet.broadcastTx(raw, network) })
+    })
+    .get(URL_BASE + ":network/block/:hash", async (req: Request, res: Response, next: NextFunction) => {
+        const hash = req.params.hash
+        const network = req.params.network
+        const hashValid = validateHash(hash)
+
+        if (hashValid.error)
+            res.status(hashValid.code).json({ message: hashValid.message })
+        else
+            res.status(200).json(await wallet.getBlock(hash, network))
     })
 
 start()
