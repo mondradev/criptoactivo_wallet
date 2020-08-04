87c27d8479acdfeaf41c640751db51260d94cb21
Ing. Javier Flores <jjflores@innsytech.com>
2020-08-04 12:36:44 -0500

    feat(android): Se crean las excepciones de las operaciones crypto

---
+++ CryptoWalletApp/app/src/main/java/com/cryptowallet/assets/bitcoin/wallet/exceptions/OverflowException.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2020. Criptoactivo
+ * Copyright © 2020. InnSy Tech
+ * Copyright © 2020. Ing. Javier de Jesús Flores Mondragón
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cryptowallet.assets.bitcoin.wallet.exceptions;
+
+
+import android.content.res.Resources;
+
+import com.cryptowallet.R;
+import com.cryptowallet.wallet.SupportedAssets;
+import com.cryptowallet.wallet.exceptions.InvalidAmountException;
+
+import org.jetbrains.annotations.NotNull;
+
+
+/**
+ * Excepción de cantidad desborde, que ocurre cuando una cantidad supera el máximo permitido en una
+ * una transacción de Bitcoin.
+ *
+ * @author Ing. Javier Flores (jjflores@innsytech.com)
+ * @version 1.0
+ */
+public class OverflowException extends InvalidAmountException {
+
+    /**
+     * Crea una instancia de la excepción.
+     */
+    public OverflowException(long amount) {
+        super(SupportedAssets.BTC, amount);
+    }
+
+    /**
+     * Obtiene el identificador del mensaje de error de la excepción.
+     *
+     * @return Identificador del recurso.
+     */
+    @Override
+    public String getMessageRes(@NotNull Resources resources) {
+        return resources.getString(R.string.max_value_error,
+                getCryptoAsset().toStringFriendly(getAmount()));
+    }
+}
