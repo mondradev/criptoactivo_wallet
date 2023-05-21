e55c5eb1d467c34dbd5c92e3c8d91b0784b74940
Javier de Jesús Flores Mondragón <jjflores.mondra@outlook.com>
2023-05-21 07:41:51 -0600

    chore: convert into kotlin script

--- CryptoWalletApp/settings.gradle.kts
+++ CryptoWalletApp/settings.gradle.kts
@@ -15,8 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-pluginManagement {
+import org.gradle.kotlin.dsl.*
 
+settings {
     /**
      * The pluginManagement {repositories {...}} block configures the
      * repositories Gradle uses to search or download the Gradle plugins and
@@ -26,33 +27,33 @@ pluginManagement {
      * defines the Gradle Plugin Portal, Google's Maven repository,
      * and the Maven Central Repository as the repositories Gradle should use to look for its dependencies.
      */
-
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
-}
-dependencyResolutionManagement {
 
-    /**
-     * The dependencyResolutionManagement { repositories {...}}* block is where you configure the repositories and dependencies used by
-     * all modules in your project, such as libraries that you are using to
-     * create your application. However, you should configure module-specific
-     * dependencies in each module-level build.gradle file. For new projects,
-     * Android Studio includes Google's Maven repository
-     * and the Maven Central Repository by
-     * default, but it does not configure any dependencies (unless you select a
-     * template that requires some).
-     */
-
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
+    dependencyResolutionManagement {
+        /**
+         * The dependencyResolutionManagement { repositories {...}}* block is where you configure the repositories and dependencies used by
+         * all modules in your project, such as libraries that you are using to
+         * create your application. However, you should configure module-specific
+         * dependencies in each module-level build.gradle file. For new projects,
+         * Android Studio includes Google's Maven repository
+         * and the Maven Central Repository by
+         * default, but it does not configure any dependencies (unless you select a
+         * template that requires some).
+         */
+        repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+        repositories {
+            google()
+            mavenCentral()
+        }
     }
 }
+
 rootProject.name = "CryptoWalletApp"
-include ":app"
-include ':bitcoin'
-include ':core'
+
+include(":app")
+include(":bitcoin")
+include(":core")
